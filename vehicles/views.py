40f1b4c57b36d1f86f0f3dd43f67696bfcc0ba54
Stefan Ashugyan <abrams1234518@gmail.com>
2024-04-12 18:47:34 +0400

    Added single_page(info) and connect front with home-view

--- vehicles/views.py
+++ vehicles/views.py
@@ -3,14 +3,17 @@ from django.shortcuts import render, get_object_or_404
 from .models import VehiclesForRent
 
 def home(request):
-    return render(request, 'index.html')
+    vehicles = VehiclesForRent.objects.filter(available=True)
+
+    return render(request, 'index.html', {'vehicles': vehicles})
 
 
 def contact(request):
     return render(request, 'contact.html')
 
+
+
 def single_post(request, post_id):
     post = get_object_or_404(VehiclesForRent, pk=post_id)
-    print(post)
 
-    return render(request, 'single_post.html', {'single_posts': post})
+    return render(request, 'single_post.html', {'single_post': post})
